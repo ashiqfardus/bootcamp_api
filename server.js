fc3df20b3ca3718a3fe2f465d798c3ce0b94f8cd
ashiqfardus <ashiqfardus@hotmail.com>
2022-11-22 22:56:25 +0600

    bootcamps route files added

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//Route files
+const bootcamp = require('./routes/bootcamps');
+
+const {flatten} = require("express/lib/utils");
 
 //Load env vars
 dotenv.config({path:'./config/config.env'});
 
 const app = express();
 
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamp);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, ()=>{
