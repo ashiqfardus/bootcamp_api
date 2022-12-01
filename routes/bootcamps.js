5ff3cf27786da4744731b0b612bd274e0f7e5b1c
ashiqfardus <ashiqfardus@hotmail.com>
2022-12-02 00:46:59 +0600

    Advanced filtering middleware added for every model

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
     bootcampPhotoUpload
 } = require('../controllers/bootcamps');
 
+const Bootcamp = require("../models/Bootcamp");
+const advancedResults = require('../middleware/advancedResults');
+
 //Include other resource router
 const courseRouter = require('./courses');
 
@@ -22,7 +25,7 @@ router
 
 router
     .route('/')
-    .get(getBootcamps)
+    .get(advancedResults(Bootcamp, 'courses'),getBootcamps)
     .post(createBootcamp);
 
 router
