d8fa5aa7d036727abac6039a8250f145edecc185
ashiqfardus <ashiqfardus@hotmail.com>
2022-12-11 00:13:05 +0600

    password reset token created and send sent

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const ErrorResponse =require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
+const sendEmail = require('../utils/sendEmail');
 
 //@desc     Register user
 //@route    POST /api/v1/auth/register
@@ -44,6 +45,68 @@ exports.login = asyncHandler(async (req, res, next)=>{
     sendTokenResponse(user,200,res);
 });
 
+//@desc     Get current logged-in user
+//@route    POST /api/v1/auth/me
+//@access   private
+
+exports.getMe = asyncHandler(async (req,res,next)=>{
+    const user = await User.findById(req.user.id);
+    res.status(200).json({
+        success:true,
+        data:user
+    })
+
+});
+
+
+//@desc     Forgot password
+//@route    POST /api/v1/auth/forgotpassword
+//@access   public
+
+exports.forgotPassword = asyncHandler(async (req,res,next)=>{
+    const user = await User.findOne({email: req.body.email});
+    if (!user){
+        return next(new ErrorResponse(`No user found.`, 404));
+    }
+
+    //Get reset token
+    const resetToken = user.getResetPasswordToken();
+
+    await user.save({
+       validateBeforeSave:false,
+    });
+
+    //Create reseet URL
+    const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+
+    const message = `Please make a PUT request to: \n\n ${resetUrl}`;
+
+    try{
+        await sendEmail({
+           email:user.email,
+            subject:'Password reset',
+            message
+        });
+        res.status(200).json({
+            success:true,
+            data:"Email sent"
+        })
+    }
+    catch (e){
+        console.log(e);
+        user.resetPasswordToken =undefined;
+        user.resetPasswordExpire =undefined;
+
+        await user.save({
+            validateBeforeSave:false
+        });
+
+        return next(new ErrorResponse(`Email not sent.`, 500));
+    }
+});
+
+
+
 //Get token from model, create cookie send response
 const sendTokenResponse = (user, statusCode, res)=>{
     //create token
@@ -66,16 +129,3 @@ const sendTokenResponse = (user, statusCode, res)=>{
             token
         })
 }
-
-//@desc     Get current logged-in user
-//@route    POST /api/v1/auth/me
-//@access   private
-
-exports.getMe = asyncHandler(async (req,res,next)=>{
-    const user = await User.findById(req.user.id);
-    res.status(200).json({
-        success:true,
-        data:user
-    })
-
-})
